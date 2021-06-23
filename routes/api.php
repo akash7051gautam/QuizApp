7dfedcda3269bdde563774836958ab20016e5846
abhishekpathania1 <abhishekpathaniawins@gmail.com>
2021-06-23 14:51:02 +0530

    Created Students api

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,9 @@
 <?php
 
-<<<<<<< Updated upstream
 
-=======
-use App\Http\Controllers\StudentController;
->>>>>>> Stashed changes
+
+
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,30 +21,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-<<<<<<< Updated upstream
-
-
-Route::get('/apiResource', 'QuizeController@index');
-Route::post('/apiResource', 'QuizeController@store');
-Route::get('/apiResource/{id}', 'QuizeController@show');
-Route::put('/apiResource/{id}', 'QuizeController@update');
-Route::delete('/apiResource/{id}', 'QuizeController@destroy');
-
-
-
-Route::get('question','QuestionController@index');
-Route::post('question','QuestionController@store');
-Route::get('question/{id}','QuestionController@show');
-Route::put('question/{id}','QuestionController@update');
-Route::delete('question/{id}','QuestionController@destroy');
-
-Route::get('option','OptionController@index');
-Route::post('option','OptionController@store');
-Route::get('option/{id}','OptionController@show');
-Route::put('option/{id}','OptionController@update');
-Route::delete('option/{id}','OptionController@destroy');
-
-
-Route::get('join','JoinTableController@index');
-=======
->>>>>>> Stashed changes
